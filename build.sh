3943ff05da384a0f62b32dccbdc556a56dabaf2f
openwrt2223 <92192602+openwrt2223@users.noreply.github.com>
2022-08-25 12:30:57 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ sudo timedatectl set-timezone "$TZ"
 sudo mkdir -p /yunbuilduboot
 sudo chown $USER:$GROUPS /yunbuilduboot
 cd /yunbuilduboot/
-wget https://downloads.openwrt.org/snapshots/targets/ramips/mt7620/openwrt-toolchain-ramips-mt7620_gcc-11.2.0_musl.Linux-x86_64.tar.xz -O openwrt-toolchain-ramips-mt7620_gcc-11.2.0_musl.Linux-x86_64.tar.xz
-tar -xvf openwrt-toolchain-ramips-mt7620_gcc-11.2.0_musl.Linux-x86_64.tar.xz
-rm -rf openwrt-toolchain-ramips-mt7620_gcc-11.2.0_musl.Linux-x86_64.tar.xz
+wget https://downloads.openwrt.org/snapshots/targets/ramips/mt7620/openwrt-toolchain-ramips-mt7620_gcc-11.3.0_musl.Linux-x86_64.tar.xz -O openwrt-toolchain-ramips-mt7620_gcc-11.3.0_musl.Linux-x86_64.tar.xz
+tar -xvf openwrt-toolchain-ramips-mt7620_gcc-11.3.0_musl.Linux-x86_64.tar.xz
+rm -rf openwrt-toolchain-ramips-mt7620_gcc-11.3.0_musl.Linux-x86_64.tar.xz
 git clone --depth=1 -b youku-l1 https://github.com/openwrt2223/u-boot.git
-export STAGING_DIR=/yunbuilduboot/openwrt-toolchain-ramips-mt7620_gcc-11.2.0_musl.Linux-x86_64/
-export PATH=$STAGING_DIR/toolchain-mipsel_24kc_gcc-11.2.0_musl/bin:$PATH
+export STAGING_DIR=/yunbuilduboot/openwrt-toolchain-ramips-mt7620_gcc-11.3.0_musl.Linux-x86_64/
+export PATH=$STAGING_DIR/toolchain-mipsel_24kc_gcc-11.3.0_musl/bin:$PATH
 cd u-boot
 make CROSS_COMPILE=mipsel-openwrt-linux- mt7620_rfb_defconfig
 make CROSS_COMPILE=mipsel-openwrt-linux-
