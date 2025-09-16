7929e09b4fd56fd4977307f1b6c83d5e8efe6783
openwrt2223 <92192602+openwrt2223@users.noreply.github.com>
2025-09-17 07:41:20 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc
 sudo apt update
-sudo apt install wget curl git tar zip unzip screen clang make bison flex python3-distutils swig python3-dev libssl-dev -y
+sudo apt install wget curl git tar zip unzip zstd screen make bison flex -y
 sudo apt autoremove -y
 sudo apt clean
 sudo timedatectl set-timezone "$TZ"
 sudo mkdir -p /yunbuilduboot
 sudo chown $USER:$GROUPS /yunbuilduboot
 cd /yunbuilduboot/
-wget https://downloads.openwrt.org/snapshots/targets/ramips/mt7620/openwrt-toolchain-ramips-mt7620_gcc-14.3.0_musl.Linux-x86_64.tar.xz -O openwrt-toolchain-ramips-mt7620_gcc-14.3.0_musl.Linux-x86_64.tar.xz
-tar -xvf openwrt-toolchain-ramips-mt7620_gcc-14.3.0_musl.Linux-x86_64.tar.xz
-rm -rf openwrt-toolchain-ramips-mt7620_gcc-14.3.0_musl.Linux-x86_64.tar.xz
+wget https://downloads.openwrt.org/snapshots/targets/ramips/mt7620/openwrt-toolchain-ramips-mt7620_gcc-14.3.0_musl.Linux-x86_64.tar.zst -O openwrt-toolchain-ramips-mt7620_gcc-14.3.0_musl.Linux-x86_64.tar.zst
+tar --use-compress-program=unzstd -xvf openwrt-toolchain-ramips-mt7620_gcc-14.3.0_musl.Linux-x86_64.tar.zst
+rm -rf openwrt-toolchain-ramips-mt7620_gcc-14.3.0_musl.Linux-x86_64.tar.zst
 git clone --depth=1 -b youku-l1 https://github.com/openwrt2223/u-boot.git
-export STAGING_DIR=/yunbuilduboot/openwrt-toolchain-ramips-mt7620_gcc-11.3.0_musl.Linux-x86_64/
-export PATH=$STAGING_DIR/toolchain-mipsel_24kc_gcc-11.3.0_musl/bin:$PATH
+export STAGING_DIR=/yunbuilduboot/openwrt-toolchain-ramips-mt7620_gcc-14.3.0_musl.Linux-x86_64/
+export PATH=$STAGING_DIR/toolchain-mipsel_24kc_gcc-14.3.0_musl/bin:$PATH
 cd u-boot
 make CROSS_COMPILE=mipsel-openwrt-linux- mt7620_rfb_defconfig
 make CROSS_COMPILE=mipsel-openwrt-linux-
